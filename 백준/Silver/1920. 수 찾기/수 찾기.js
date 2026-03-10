cd8ec6b47deedc751c52a81e3290843df1b5acdc
tw200257 <taewoong20025721@gmail.com>
2026-03-11 01:01:47 +0900

    [Silver IV] Title: 수 찾기, Time: 260 ms, Memory: 44244 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const nArray = input[1].split(' ').map(Number); //입력받은 숫자들
+const mArray = input[3].split(' ').map(Number); //찾아야 할 숫자들
+const nSet = new Set(nArray);
+const r = mArray.map(num => {
+    return nSet.has(num) ? 1 : 0; 
+    
+});
+console.log(r.join('\n'));
