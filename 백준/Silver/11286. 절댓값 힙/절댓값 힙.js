640edffc0c6e9fd04df9bcdfd3fd7ffe3607007e
tw200257 <taewoong20025721@gmail.com>
2026-03-25 15:22:22 +0900

    [Silver I] Title: 절댓값 힙, Time: 232 ms, Memory: 27460 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.js
@@ -0,0 +1,70 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().split("\n");
+const N = parseInt(input[0]);
+const op = input.slice(1).map(Number);
+
+class AbsHeap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  compare(a, b) {
+    const absA = Math.abs(a);
+    const absB = Math.abs(b);
+    if (absA === absB) {
+      return a < b;
+    }
+    return absA < absB;
+  }
+
+  push(value) {
+    this.heap.push(value);
+    let cur = this.heap.length - 1;
+    while (cur > 1) {
+      let parent = Math.floor(cur / 2);
+      if (this.compare(this.heap[cur], this.heap[parent])) {
+        [this.heap[cur], this.heap[parent]] = [this.heap[parent], this.heap[cur]];
+        cur = parent;
+      } else break;
+    }
+  }
+
+  pop() {
+    if (this.heap.length <= 1) return 0;
+    if (this.heap.length === 2) return this.heap.pop();
+
+    const top = this.heap[1];
+    this.heap[1] = this.heap.pop();
+    let cur = 1;
+
+    while (cur * 2 < this.heap.length) {
+      let left = cur * 2;
+      let right = cur * 2 + 1;
+      let target = left;
+
+      if (right < this.heap.length && this.compare(this.heap[right], this.heap[left])) {
+        target = right;
+      }
+
+      if (this.compare(this.heap[target], this.heap[cur])) {
+        [this.heap[cur], this.heap[target]] = [this.heap[target], this.heap[cur]];
+        cur = target;
+      } else break;
+    }
+    return top;
+  }
+}
+
+const heap = new AbsHeap();
+let result = [];
+
+for (let i = 0; i < N; i++) {
+  const x = op[i];
+  if (x === 0) {
+    result.push(heap.pop());
+  } else {
+    heap.push(x);
+  }
+}
+
+console.log(result.join("\n"));
